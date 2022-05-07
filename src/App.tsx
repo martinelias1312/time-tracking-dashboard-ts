d777822b8bccc24cca438c2b1aaabf8ed2578e61
smradupan <martinelias630@gmail.com>
2022-05-07 11:03:51 +0200

    initial files, starting state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <footer className="attribution">
+        Challenge by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.frontendmentor.io?ref=challenge"
+          rel="noreferrer"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by{" "}
+        <a href="https://www.linkedin.com/in/martin-eli%C3%A1%C5%A1-455550209/">
+          Martin Eliáš
+        </a>
+        .
+      </footer>
     </div>
   );
 }
