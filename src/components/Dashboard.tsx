19460a53b474cabf2d2c2a133dfda5e69c7d1481
smradupan <martinelias630@gmail.com>
2022-05-07 23:14:36 +0200

    data imported from parent, dashboard html layout

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,20 +1,62 @@
 import React from "react";
 
 // styles
+import "./Dashboard.scss";
 
-// components
-import StatsCards from "./StatsCards";
-
-type Props = {};
+type Props = {
+  data: {
+    id: number;
+    title: string;
+    timeframes: {
+      daily: {
+        current: number;
+        previous: number;
+      };
+      weekly: {
+        current: number;
+        previous: number;
+      };
+      monthly: {
+        current: number;
+        previous: number;
+      };
+    };
+  }[];
+};
 
 const Dashboard = (props: Props) => {
-  let data = require("../data.json");
-  console.log(data);
-
+  const { data } = props;
   return (
-    <section className="stats-dashboard">
-      <StatsCards />
-    </section>
+    <ul className="stats-dashboard">
+      {data.map((data) => (
+        <li key={data.id.toString()} className={data.title.toLocaleLowerCase()}>
+          <img
+            src={"../assets/images/icon-" + data.title.toLowerCase() + ".svg"}
+            alt=""
+            aria-hidden="true"
+          />
+          <div className="stats">
+            <div className="stats-heading">
+              <h2>{data.title}</h2>
+              <img
+                src="../assets/images/icon-ellipsis.svg"
+                alt=""
+                aria-hidden="true"
+              />
+            </div>
+
+            <div className="time">
+              <p className="time-current">
+                {data.timeframes.weekly.current}hrs
+              </p>
+              <p className="time-previous">
+                Last Week - {data.timeframes.weekly.previous}hrs
+              </p>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 };
 
