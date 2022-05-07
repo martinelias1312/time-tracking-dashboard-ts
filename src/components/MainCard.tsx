49d729e7524af04b8571e24335b990892b50c2b7
smradupan <martinelias630@gmail.com>
2022-05-07 11:53:35 +0200

    maincard components added

---
+++ src/components/MainCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const MainCard = (props: Props) => {
+  return <div>MainCard</div>;
+};
+
+export default MainCard;
---
+++ src/components/MainCardNav.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const MainCardNav = (props: Props) => {
+  return <div>MainCardNav</div>;
+};
+
+export default MainCardNav;
