46b08cffab3ce9e22ce63af0005bb43ee81d4997
smradupan <martinelias630@gmail.com>
2022-05-07 12:01:54 +0200

    main card html layout

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 
+// components
+import MainCardNav from "./MainCardNav";
+
+// img
+import person from "../assets/images/image-jeremy.png";
+
 type Props = {};
 
 const MainCard = (props: Props) => {
-  return <div>MainCard</div>;
+  return (
+    <div className="main-card">
+      <figure className="person-card">
+        <img src={person} alt="Person face" className="person-img" />
+        <figcaption>
+          Report for<p className="person-name">Jeremy Robson</p>
+        </figcaption>
+      </figure>
+
+      <MainCardNav />
+    </div>
+  );
 };
 
 export default MainCard;
