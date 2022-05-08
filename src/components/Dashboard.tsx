07755222d23a2de3944cd15cc7cd2e807246c95a
smradupan <martinelias630@gmail.com>
2022-05-08 12:10:13 +0200

    img replaced for svg

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -34,12 +34,18 @@ const Dashboard = (props: Props) => {
             <div className="stats-heading">
               <h2>{data.title}</h2>
               <div className="dots-wrap">
-                <img
-                  src={require("../assets/images/icon-ellipsis.svg").default}
-                  alt=""
-                  aria-hidden="true"
+                <svg
+                  width="21"
+                  height="5"
                   className="img-dots"
-                />
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+                    fill="#BBC0FF"
+                    fill-rule="evenodd"
+                  />
+                </svg>
               </div>
             </div>
 
