929217155c4e02b74931a7621d3be65813b9b6df
= <martinelias630@gmail.com>
2022-05-09 00:51:20 +0200

    button click event functions

--- src/components/MainCardNav.tsx
+++ src/components/MainCardNav.tsx
@@ -3,30 +3,41 @@ import React, { MouseEvent } from "react";
 // styles
 import "./MainCardNav.scss";
 
-type Props = {};
+type Props = {
+  switchTimeMode: (arg: string) => void;
+  timeMode: string;
+};
 
 // component
 const MainCardNav = (props: Props) => {
-  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
-    let buttons = document.querySelectorAll(".time-btn");
-    buttons.forEach((butt) => butt.classList.remove("active"));
-
+  const setActive = (e: MouseEvent<HTMLButtonElement>) => {
+    // click on btn and add active class, reset active on others
+    const buttons = document.querySelectorAll(".time-btn");
+    buttons.forEach((btn) => btn.classList.remove("active"));
     e.currentTarget.classList.add("active");
+
+    // check if clicked item has class with day/week/month and pass this class to switchTimeMode function
+    if (e.currentTarget.classList[0] === "day") {
+      props.switchTimeMode("day");
+    } else if (e.currentTarget.classList[0] === "week") {
+      props.switchTimeMode("week");
+    } else {
+      props.switchTimeMode("month");
+    }
   };
   // template
   return (
     <div className="main-card-nav">
-      <button onClick={handleClick} className="time-btn">
+      <button onClick={setActive} className="day time-btn">
         Daily
       </button>
-      <button onClick={handleClick} className="time-btn">
+      <button onClick={setActive} className="week time-btn">
         Weekly
       </button>
-      <button onClick={handleClick} className="time-btn">
+      <button onClick={setActive} className="month time-btn">
         Monthly
       </button>
     </div>
   );
 };
-
 export default MainCardNav;
